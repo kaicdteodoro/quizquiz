f441f0860ab6e87fb22f7f2f9be874ff5a1ffc4d
kaicdteodoro <kaic.teodoro@mazzafc.tech>
2023-06-19 23:03:49 -0300

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,30 @@
+import './bootstrap';
+import { createApp } from 'vue';
+import App from './components/App.vue';
+
+// Vuetify
+import 'vuetify/styles';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'dark'
+  },
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
+
+const app = createApp(App);
+
+app.use(vuetify);
+app.mount('#app');
